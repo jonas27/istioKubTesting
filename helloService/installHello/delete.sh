93038e2b58c68a62824befef81c0ee07168a9ec2
jonas27 <jonas.burster@gmail.com>
2019-04-09 15:30:19 +0200

    push

--- helloService/installHello/delete.sh
+++ helloService/installHello/delete.sh
@@ -1,4 +1,4 @@
-kubectl delete -f namespace.yaml --ignore-not-found && \
-kubectl delete -f destRules.yaml && \
-kubectl delete -f gateway.yaml && \
 kubectl delete -f helloManifest.yaml
+kubectl delete -f destRules.yaml
+kubectl delete -f gateway.yaml
+kubectl delete -f namespace.yaml --ignore-not-found
--- helloService/installHello/install.sh
+++ helloService/installHello/install.sh
@@ -1,11 +1,12 @@
-kubectl apply -f namespace.yaml && \
-kubectl label namespace hello istio-injection=enabled && \
-kubectl apply -f destRules.yaml && \
-kubectl apply -f gateway.yaml && \
+#kubectl apply -f namespace.yaml
+#kubectl label namespace hello istio-injection=enabled
+kubectl label namespace default istio-injection=enabled
 kubectl apply -f helloManifest.yaml
+#kubectl apply -f destRules.yaml
+kubectl apply -f gateway.yaml
 
-echo "*** wait 30 seconds for pods to start ***"
-sleep 30
+echo "*** wait 60 seconds for pods to start ***"
+sleep 60
 
 echo "*** Now test: ***"
-curl -o /dev/null -s -w "%{http_code}\n" http://0.0.0.0:31380/hello/
+curl -o /dev/null -s -w "%{http_code}\n" http://0.0.0.0:31380/hello/hello
